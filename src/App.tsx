fd77092f0c2b8e918f6d85469987455c3d91507e
mehm8128 <mehm8128@gmail.com>
2021-12-04 15:34:49 +0900

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.module.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
 
 function App() {
 	return (
 		<div className="App">
-			<Home />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	)
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,39 @@
-import React from "react"
+import { useState } from "react"
 import "./Login.module.css"
 
 function Login() {
+	const [userName, setUserName] = useState("")
+	const [passWord, setPassWord] = useState("")
+
 	function handleSubmit() {
-		alert("ログインしました")
+		alert("POSTする")
+	}
+	function handleUserNameChange(e: any) {
+		setUserName(e.target.value)
+	}
+	function handlePassWordChange(e: any) {
+		setPassWord(e.target.value)
 	}
 	return (
 		<div className="Login">
 			<span>新規登録orログイン</span>
 			<form onSubmit={handleSubmit}>
-				<input type="text" name="userName" placeholder="ユーザー名" />
-				<input type="text" name="passWord" />
+				<input
+					type="text"
+					name="userName"
+					placeholder="ユーザー名"
+					onChange={handleUserNameChange}
+				/>
+				<input
+					type="text"
+					name="passWord"
+					placeholder="パスワード"
+					onChange={handlePassWordChange}
+				/>
 				<input type="submit" value="submit" />
 			</form>
 		</div>
 	)
 }
-
+//stateに入れてボタン押したときにPOST
 export default Login
