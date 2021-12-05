6ebb5ced1d35af21501f98f09b4be893abcc97d7
mehm8128 <mehm8128@gmail.com>
2021-12-05 19:24:34 +0900

    sampleを変更

--- src/pages/Memorize.tsx
+++ src/pages/Memorize.tsx
@@ -20,27 +20,40 @@ interface Pin {
 function Memorize() {
 	const [flag, setFlag] = useState([...Array(2)].fill(false)) //2のところは取得したembededPinsの数を充てる
 	const params = useParams()
-	//paramsが宮殿のidなのでaxiosで宮殿情報取得
-	const [samplePalace, setSamplePalace] = useState({
-		id: "0",
-		name: "Versailles",
-		image: palace1,
-		embededPins: [
-			{ id: "a1", x: 0, y: 0, word: "apple", memo: "aaa" },
-			{ id: "a2", x: 1, y: 1, word: "banana", memo: "bbb" },
-		],
-	})
-	const listItems = samplePalace.embededPins.map((pin, index) => (
-		<li>
-			<Word
-				key={index}
-				word={pin.word}
-				num={index}
-				flag={flag}
-				setFlag={setFlag}
-			/>
-		</li>
-	))
+	//params.idが宮殿のidなのでaxiosで宮殿情報取得
+	const [samplePalace, setSamplePalace] = useState([
+		{
+			id: "0",
+			name: "Versailles",
+			image: palace1,
+			embededPins: [
+				{ id: "a1", x: 0, y: 0, word: "apple", memo: "aaa" },
+				{ id: "a2", x: 1, y: 1, word: "banana", memo: "bbb" },
+			],
+		},
+		{
+			id: "1",
+			name: "Buckingham",
+			image: palace2,
+			embededPins: [
+				{ id: "a1", x: 0, y: 0, word: "apple", memo: "aaa" },
+				{ id: "a2", x: 1, y: 1, word: "banana", memo: "bbb" },
+			],
+		},
+	])
+	const listItems = samplePalace[Number(params.id)].embededPins.map(
+		(pin, index) => (
+			<li>
+				<Word
+					key={index}
+					word={pin.word}
+					num={index}
+					flag={flag}
+					setFlag={setFlag}
+				/>
+			</li>
+		)
+	)
 
 	function handleClick() {
 		alert("ダイアログ表示")
@@ -53,11 +66,10 @@ function Memorize() {
 			{
 				//<img src={} alt={location.state.palace.name} />
 			}
-			{params.id === "0" ? (
-				<img src={palace1} alt={samplePalace.name} />
-			) : (
-				<img src={palace2} alt={samplePalace.name} />
-			)}
+			<img
+				src={samplePalace[Number(params.id)].image}
+				alt={samplePalace[Number(params.id)].name}
+			/>
 			{/*あとでコンポーネント分ける*/}
 			<ol>{listItems}</ol>
 			<br />
