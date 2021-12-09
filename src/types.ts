123b499c4557f51e04cf937006a7afb88a1869cc
ucciqun <zerda.elsword@gmail.com>
2021-12-09 16:45:39 +0900

    add registration types

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,37 @@
 //PalaceType
 type EmbededPins = {
-	number: number
-	x: number
-	y: number
-	word: string
-	memo: string
+  number: number
+  x: number
+  y: number
+  word: string
+  memo: string
 }
 
 export type PalaceType = {
-	id: string
-	name: string
-	image: string
-	embededPins: EmbededPins[]
+  id: string
+  name: string
+  image: string
+  embededPins: EmbededPins[]
 }
 //TemplateType
 type Pins = {
-	number: number
-	x: number
-	y: number
+  number: number
+  x: number
+  y: number
 }
 
 export type TemplateType = {
-	id: string
-	name: string
-	image: string
-	pins: Pins[]
-	createdBy: string
+  id: string
+  name: string
+  image: string
+  pins: Pins[]
+  createdBy: string
+}
+export type UserRegistration = {
+  name: string
+  password: string
+}
+export type RegistrationResponse = {
+  id: string
+  name: string
 }
