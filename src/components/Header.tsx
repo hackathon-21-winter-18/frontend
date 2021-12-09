b4e3133f740c764c1402b7126a965988bce26e14
mehm8128 <mehm8128@gmail.com>
2021-12-09 22:13:35 +0900

    whoamI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,17 @@ import {Link, useNavigate} from 'react-router-dom'
 import logo from '../assets/脳みそ.png'
 import FromNewPalace from './DialogFromNewPalace'
 import {UserContext} from './UserProvider'
+import axios from 'axios'
 
 const Header: React.VFC = () => {
   const {user, logout} = useContext(UserContext)
   const navigate = useNavigate()
+  function handleGet() {
+    axios.get('http://localhost:8080/api/oauth/whoamI').then((res) => {
+      console.log(res.data)
+      console.log(res.data.id, res.data.name)
+    })
+  }
   return (
     <div className={styles.header}>
       <img src={logo} alt="logo" width="2%" onClick={() => navigate('/')} />
@@ -25,6 +32,7 @@ const Header: React.VFC = () => {
           <Link to="login">ログイン</Link>
         </>
       )}
+      <button onClick={handleGet}>ボタン</button>
     </div>
   )
 }
