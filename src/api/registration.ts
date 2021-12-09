b764c93e65d5611fc21ecfdf45c9102d735e9364
mehm8128 <mehm8128@gmail.com>
2021-12-09 21:18:49 +0900

    間違えてmasterにpushしちゃったので戻しました

--- src/api/registration.ts
+++ src/api/registration.ts
@@ -4,10 +4,8 @@ import {RegistrationResponse, UserRegistration} from '../types'
 const endpoint = 'http://localhost:8080'
 
 export const postLogin = async (user: UserRegistration) => {
-  const res = await axios.post<RegistrationResponse>(endpoint + '/api/oauth/login', user, {withCredentials: true})
+  const res = await axios.post<RegistrationResponse>(endpoint + '/api/oauth/login', user)
   // TODO response error handling
-  console.log(res.headers['set-cookie'])
-  console.log(res.headers)
   return res.data
 }
 
