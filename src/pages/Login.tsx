4c7b6e4b485e4ae8731fbeb5495fa217f3ee9fbe
ucciqun <zerda.elsword@gmail.com>
2021-12-09 20:10:45 +0900

    fix login component layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,28 +2,47 @@ import React, {useState} from 'react'
 import styles from './Login.module.css'
 import {useContext} from 'react'
 import {UserContext} from '../components/UserProvider'
+import {Link} from 'react-router-dom'
 
 const Login: React.VFC = () => {
-  const [userName, setUserName] = useState('')
-  const [passWord, setPassWord] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
   const {login} = useContext(UserContext)
 
   const handleLogin = async (e: React.MouseEvent) => {
     e.preventDefault()
-    await login({name: userName, password: passWord})
-    setUserName('')
-    setPassWord('')
+    if (name === '' || password === '') return
+
+    await login({name, password})
+    setName('')
+    setPassword('')
   }
 
   return (
-    <div>
-      <form>
-        <input type="text" name="userName" placeholder="ユーザー名" onChange={(e) => setUserName(e.target.value)} />
-        <br />
-        <input type="text" name="passWord" placeholder="パスワード" onChange={(e) => setPassWord(e.target.value)} />
-        <br />
-        <button onClick={handleLogin}>ログイン</button>
-      </form>
+    <div className={styles.layout}>
+      <div className={styles.login}>
+        <h1>ログイン</h1>
+        <div className={styles.divider} />
+        <h2>おかえりなさい。</h2>
+        <form className={styles.form}>
+          <input type="text" name="name" placeholder="ユーザー名" onChange={(e) => setName(e.target.value)} required />
+          <input
+            type="text"
+            name="password"
+            placeholder="パスワード"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div className={styles.buttonContainer}>
+            <Link className={styles.signupButton} to="/signup">
+              アカウントを作成する
+            </Link>
+            <button className={styles.loginButton} onClick={handleLogin} type="submit" disabled={!name || !password}>
+              ログイン
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
