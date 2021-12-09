bd68311efebfe8c6ac7c98a8563edc3f607eb9cf
mehm8128 <mehm8128@gmail.com>
2021-12-09 21:20:12 +0900

    CORS policyエラー

--- src/api/registration.ts
+++ src/api/registration.ts
@@ -4,8 +4,10 @@ import {RegistrationResponse, UserRegistration} from '../types'
 const endpoint = 'http://localhost:8080'
 
 export const postLogin = async (user: UserRegistration) => {
-  const res = await axios.post<RegistrationResponse>(endpoint + '/api/oauth/login', user)
+  const res = await axios.post<RegistrationResponse>(endpoint + '/api/oauth/login', user, {withCredentials: true})
   // TODO response error handling
+  console.log(res.headers['set-cookie'])
+  console.log(res.headers)
   return res.data
 }
 
