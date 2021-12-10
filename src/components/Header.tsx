238a3e1e09002cfcfc7b57613bacdaf494d4881a
ucciqun <zerda.elsword@gmail.com>
2021-12-10 20:32:24 +0900

    ロゴをページに追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import {useContext} from 'react'
 import styles from './Header.module.css'
 import {Link, useNavigate} from 'react-router-dom'
-import logo from '../assets/脳みそ.png'
+import logo from '../assets/logo.svg'
 import FromNewPalace from './DialogFromNewPalace'
 import {UserContext} from './UserProvider'
 
@@ -10,19 +10,14 @@ const Header: React.VFC = () => {
   const navigate = useNavigate()
   return (
     <div className={styles.header}>
-      <img src={logo} alt="logo" width="2%" onClick={() => navigate('/')} />
-      <span>Palamo</span>
-      {user.auth ? (
+      {user.auth && (
         <>
+          <img className={styles.logo} src={logo} alt="logo" onClick={() => navigate('/')} />
           <span>{user.name}でログイン中</span>
           <FromNewPalace />
           <Link to="login" onClick={logout}>
             ログアウト
           </Link>
-        </>
-      ) : (
-        <>
-          <Link to="login">ログイン</Link>
         </>
       )}
     </div>
