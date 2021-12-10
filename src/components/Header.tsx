2e19e82059769f05e3fd485c841f853bf2c9f858
ucciqun <zerda.elsword@gmail.com>
2021-12-10 20:35:22 +0900

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,22 @@
 import {useContext} from 'react'
 import styles from './Header.module.css'
-import {Link, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import logo from '../assets/logo.svg'
 import FromNewPalace from './DialogFromNewPalace'
 import {UserContext} from './UserProvider'
 
 const Header: React.VFC = () => {
-  const {user, logout} = useContext(UserContext)
+  const {user} = useContext(UserContext)
   const navigate = useNavigate()
   return (
-    <div className={styles.header}>
+    <>
       {user.auth && (
-        <>
+        <div className={styles.header}>
           <img className={styles.logo} src={logo} alt="logo" onClick={() => navigate('/')} />
-          <span>{user.name}でログイン中</span>
           <FromNewPalace />
-          <Link to="login" onClick={logout}>
-            ログアウト
-          </Link>
-        </>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
