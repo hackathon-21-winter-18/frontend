483044edf66f70f131f0feafa4f70a0f95317a22
ucciqun <zerda.elsword@gmail.com>
2021-12-10 23:11:31 +0900

    userボタン周りを変更

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import styles from './Sidebar.module.css'
 import {useLocation} from 'react-router'
 import {Link} from 'react-router-dom'
 import HomeIcon from '@mui/icons-material/Home'
 import BalconyIcon from '@mui/icons-material/Balcony'
+import PersonPinIcon from '@mui/icons-material/PersonPin'
+import LogoutIcon from '@mui/icons-material/Logout'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import {UserContext} from './UserProvider'
+import {Menu} from '@mui/material'
 
 const Sidebar: React.VFC = () => {
   const {pathname} = useLocation()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const {user, logout} = useContext(UserContext)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <div className={styles.sidebar}>
       <Link to="/" className={pathname === '/' ? styles.buttonHere : styles.buttonNotHere}>
@@ -17,6 +32,38 @@ const Sidebar: React.VFC = () => {
         <BalconyIcon className={styles.buttonIcon} />
         My Template
       </Link>
+      <button className={styles.userSetting} onClick={handleClick}>
+        <PersonPinIcon className={styles.userIcon} />
+        {user.name}
+        <MoreVertIcon className={styles.lastIcon} />
+      </button>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        elevation={3}
+        className={styles.menu}>
+        <div className={styles.card}>
+          <p>{user.name}でログイン中</p>
+          <button
+            className={styles.logout}
+            onClick={() => {
+              handleClose()
+              logout()
+            }}>
+            <LogoutIcon />
+            ログアウト
+          </button>
+        </div>
+      </Menu>
     </div>
   )
 }
