f5874d74c1827204d7f5c7934e69e18304b296cd
itt828 <iittt828@gmail.com>
2021-12-11 00:31:26 +0900

    :bug: fix dialog

--- src/components/Palace.tsx
+++ src/components/Palace.tsx
@@ -1,11 +1,12 @@
-import {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './Palace.module.css'
 import ReactModal from 'react-modal'
-import {PalaceType} from '../types'
+import { PalaceType } from '../types'
 import axios from 'axios'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import CommentIcon from '@mui/icons-material/Comment'
+import Dialog from '@mui/material/Dialog'
 
 interface PalaceProps {
   num: number
@@ -13,24 +14,13 @@ interface PalaceProps {
   deletePalace: (number: number) => void
 }
 
-const Palace: React.VFC<PalaceProps> = ({num, palace, deletePalace}) => {
+const Palace: React.VFC<PalaceProps> = ({ num, palace, deletePalace }) => {
   const [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate()
-  const customStyles: ReactModal.Styles = {
-    // ダイアログ内のスタイル（中央に表示）
-    content: {
-      top: '30%',
-      bottom: 'auto',
-      right: 'auto',
-      left: '50%',
-    },
-    // 親ウィンドウのスタイル
-    overlay: {},
-  }
 
   function handleDelete() {
     //確認ダイアログ表示
-    axios.delete('http://localhost:8080/api/palaces/' + palace.id, {withCredentials: true})
+    axios.delete('http://localhost:8080/api/palaces/' + palace.id, { withCredentials: true })
     deletePalace(num)
   }
   function Extension() {
@@ -42,6 +32,9 @@ const Palace: React.VFC<PalaceProps> = ({num, palace, deletePalace}) => {
       case '/9j/4':
         return 'data:image/jpeg;base64,' + palace.image
     }
+  }
+  const handleDialogClose = () => {
+    setIsOpen(false);
   }
   return (
     <div className={styles.palace}>
@@ -64,14 +57,14 @@ const Palace: React.VFC<PalaceProps> = ({num, palace, deletePalace}) => {
         <CommentIcon className={styles.commentIcon} />
         {palace.embededPins.length + ' Words'}
       </div>
-      <ReactModal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
-        <Link to={'fix/' + palace.id} state={{image: 'data:image/png;base64,' + palace.image}}>
+      <Dialog open={isOpen} onClose={handleDialogClose}>
+        <Link to={'fix/' + palace.id} state={{ image: 'data:image/png;base64,' + palace.image }}>
           宮殿の編集
         </Link>
-        <br />
         <button onClick={handleDelete}>宮殿の削除</button>
-      </ReactModal>
-    </div>
+      </Dialog>
+
+    </div >
   )
 }
 
