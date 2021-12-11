d2e6c4bb2827c4b3d8f3ed55d5225b0f98d9b1b1
ucciqun <zerda.elsword@gmail.com>
2021-12-11 15:38:51 +0900

    playgroundの追加に伴って変更

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import NotFound from './pages/NotFound'
 import Layout from './components/Layout'
 import AuthenticatedRoute from './components/AuthenticatedRoute'
 import SignUp from './pages/SignUp'
+import SideLayout from './components/SideLayout'
+import Playground from './pages/Playground'
 
 function App() {
   return (
@@ -21,10 +23,13 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<SignUp />} />
               <Route element={<AuthenticatedRoute />}>
-                <Route path="/" element={<Home />} />
+                <Route element={<SideLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/template" element={<TemplatePage />} />
+                  <Route path="/playground" element={<Playground />} />
+                </Route>
                 <Route path="/memorize/:id" element={<Memorize />} />
                 <Route path="/edit/:base64/*" element={<Edit />} />
-                <Route path="/template" element={<TemplatePage />} />
                 <Route path="/*" element={<NotFound />} />
               </Route>
             </Route>
