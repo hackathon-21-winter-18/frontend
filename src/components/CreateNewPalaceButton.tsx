b9f2887f4d0f4d4cca64b175ed369f43237972f6
ucciqun <zerda.elsword@gmail.com>
2021-12-11 12:20:46 +0900

    宮殿作成ボタンを修正

--- src/components/CreateNewPalaceButton.tsx
+++ src/components/CreateNewPalaceButton.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './CreateNewPalaceButton.module.css'
-import AddIcon from '@mui/icons-material/Add'
+import img from '../assets/createPalace.png'
+import gif from '../assets/createPalace.gif'
 
 const CreateNewPalaceButton: React.VFC = () => {
   return (
     <button className={styles.createNewPalaceButton}>
-      <AddIcon className={styles.addIcon} />
+      <img src={gif} alt="" className={styles.gif} />
+      <img src={img} alt="" className={styles.img} />
       宮殿を作成する
     </button>
   )
