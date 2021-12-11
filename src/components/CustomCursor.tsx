079d7747bb42367e1f60c8c7a99ef9f96f06ecda
ucciQun <51139842+ucciqun@users.noreply.github.com>
2021-12-11 22:09:52 +0900

    hoverしてるかどうかで描画を変える

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -6,13 +6,15 @@ import pin from '../assets/pin.svg'
 type CursorType = 'default' | 'pin'
 interface CustomCursorProps {
   type: CursorType
+  isHover?: boolean
 }
 
-export const CustomCursor: React.VFC<CustomCursorProps> = ({type}) => {
+export const CustomCursor: React.VFC<CustomCursorProps> = ({type, isHover}) => {
   const {x, y} = useMousePosition()
+
   return (
     <>
-      {type === 'pin' && (
+      {type === 'pin' && (isHover === undefined || isHover === true) && (
         <div className={styles.pin} style={{left: `${x}px`, top: `${y}px`}}>
           <img src={pin} alt="" />
         </div>
