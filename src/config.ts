35b151278b77902c8f27dc7db9538cb6e86d2ffd
mehm8128 <mehm8128@gmail.com>
2021-12-30 18:59:36 +0900

    URL変更

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
 export const config = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.REACT_APP_PROD_API_URL
-  }
-  return process.env.REACT_APP_TEST_API_URL
+  return 'https://hackathon-21-winter-18.trap.show/backend'
 }
