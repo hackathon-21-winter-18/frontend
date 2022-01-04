538e7e9f561b00c3866309cbdc01ffdd8e8aca89
mehm8128 <mehm8128@gmail.com>
2022-01-04 12:16:54 +0900

    CSS修正とリファクタリング

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -3,6 +3,7 @@ import ExploreIcon from '@mui/icons-material/Explore'
 import {useState, useEffect, useRef} from 'react'
 import {getPalace} from '../api/palace'
 import {PalaceType} from '../types'
+import {useLocation} from 'react-router-dom'
 
 type quizType = {
   place: string
@@ -11,12 +12,13 @@ type quizType = {
   choice2: string
 }
 type judge = 'AC' | 'WA' | 'yet'
-//type randomNumType = 0 | 1
+type randomNumType = 0 | 1
 const Quiz: React.VFC = () => {
   const [judge, setJudge] = useState<judge>('yet')
   const [palaces, setPalaces] = useState(new Array<PalaceType>())
   const [quiz, setQuiz] = useState<quizType>({} as quizType)
-  const [randomNum, setRandomNum] = useState(0)
+  const [randomNum, setRandomNum] = useState<randomNumType>(getRandomInt(0, 1) as randomNumType)
+  const location = useLocation()
 
   function getRandomInt(min: number, max: number) {
     min = Math.ceil(min)
@@ -25,6 +27,7 @@ const Quiz: React.VFC = () => {
   }
   const ref1 = useRef<HTMLButtonElement>(null!)
   const ref2 = useRef<HTMLButtonElement>(null!)
+
   function RandomSelect(passedPalaces: PalaceType[]) {
     const palaceNum = getRandomInt(0, passedPalaces.length - 1)
     const pinNum = getRandomInt(0, passedPalaces[palaceNum].embededPins.length - 1)
@@ -32,8 +35,7 @@ const Quiz: React.VFC = () => {
     const palaceDummyNum = getRandomInt(0, passedPalaces.length - 1)
     const pinDummyNum = getRandomInt(0, passedPalaces[palaceDummyNum].embededPins.length - 1)
     const pinDummy = passedPalaces[palaceDummyNum].embededPins[pinDummyNum]
-    //const preRandomNum: randomNumType = getRandomInt(0, 1)
-    setRandomNum(getRandomInt(0, 1))
+    setRandomNum(getRandomInt(0, 1) as randomNumType)
     setQuiz({
       place: pin.place,
       situation: pin.situation,
@@ -58,53 +60,68 @@ const Quiz: React.VFC = () => {
         RandomSelect(res.data)
       }
     })
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [location]) // eslint-disable-line react-hooks/exhaustive-deps
   return (
     <div className={styles.quiz}>
-      <div className={styles.header}>
-        <ExploreIcon className={styles.buttonIcon} />
-        <span>Quick Quiz</span>
-      </div>
-      <div className={styles.question}>
-        <p className={styles.questionText}>
-          {quiz.place !== '' ? quiz.place : '場所未設定'}で{quiz.situation !== '' ? quiz.situation : '状況未設定'}のは？
-        </p>
-        <button
-          onClick={() => handleJudge(ref1.current.value)}
-          className={styles.button1}
-          ref={ref1}
-          value={quiz.choice1}>
-          {randomNum === 0
-            ? quiz.choice1 !== ''
-              ? quiz.choice1
-              : '単語未設定'
-            : quiz.choice2 !== ''
-            ? quiz.choice2
-            : '単語未設定'}
-        </button>
-        <br />
-        <span className={styles.or}>or</span>
-        <br />
-        <button
-          onClick={() => handleJudge(ref2.current.value)}
-          className={styles.button2}
-          ref={ref2}
-          value={quiz.choice2}>
-          {randomNum === 0
-            ? quiz.choice2 !== ''
-              ? quiz.choice2
-              : '単語未設定'
-            : quiz.choice1 !== ''
-            ? quiz.choice1
-            : '単語未設定'}
-        </button>
-      </div>
-      <div className={styles.answer}>
-        <span>{judge === 'AC' ? '正解!' : judge === 'WA' ? '不正解!' : null}</span>
-      </div>
-      <button onClick={() => RandomSelect(palaces)} className={styles.button3}>
-        次の問題へ
-      </button>
+      {palaces.length !== 0 ? (
+        <div>
+          <div className={styles.header}>
+            <ExploreIcon className={styles.buttonIcon} />
+            <span>Quick Quiz</span>
+          </div>
+          <div className={styles.question}>
+            <p className={styles.questionText}>
+              {quiz.place !== '' ? quiz.place : '場所未設定'}で{quiz.situation !== '' ? quiz.situation : '状況未設定'}
+              のは？
+            </p>
+            <button
+              onClick={() => handleJudge(ref1.current.value)}
+              className={styles.button1}
+              ref={ref1}
+              value={quiz.choice1}>
+              {randomNum === 0
+                ? quiz.choice1 !== ''
+                  ? quiz.choice1
+                  : '単語未設定'
+                : quiz.choice2 !== ''
+                ? quiz.choice2
+                : '単語未設定'}
+            </button>
+            <br />
+            <span className={styles.or}>or</span>
+            <br />
+            <button
+              onClick={() => handleJudge(ref2.current.value)}
+              className={styles.button2}
+              ref={ref2}
+              value={quiz.choice2}>
+              {randomNum === 0
+                ? quiz.choice2 !== ''
+                  ? quiz.choice2
+                  : '単語未設定'
+                : quiz.choice1 !== ''
+                ? quiz.choice1
+                : '単語未設定'}
+            </button>
+          </div>
+          <div className={styles.answer}>
+            <span>{judge === 'AC' ? '正解!' : judge === 'WA' ? '不正解!' : null}</span>
+          </div>
+          <button onClick={() => RandomSelect(palaces)} className={styles.button3}>
+            次の問題へ
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div className={styles.header}>
+            <ExploreIcon className={styles.buttonIcon} />
+            <span>Quick Quiz</span>
+          </div>
+          <div className={styles.question}>
+            <p className={styles.questionText}>宮殿を作成してクイズに挑戦してみましょう</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
