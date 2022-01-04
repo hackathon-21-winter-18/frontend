30a525cceefce0b59e8f27597627ca48709206a2
mehm8128 <mehm8128@gmail.com>
2022-01-04 21:18:34 +0900

    UI修正

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -1,4 +1,4 @@
-import styles from './Login.module.css'
+import styles from './Title.module.css'
 import useAuth from '../components/UserProvider'
 import {useNavigate} from 'react-router-dom'
 import logo from '../assets/logo.svg'
@@ -18,10 +18,9 @@ const Login: React.VFC = () => {
     <div className={styles.layout}>
       <div className={styles.login}>
         <img src={logo} alt="palamo logo" className={styles.logo} />
-        <h1>ログイン</h1>
         <div className={styles.divider} />
         <button className={styles.loginButton} onClick={handleLogin} type="submit">
-          ログイン
+          新規登録 or ログイン
         </button>
       </div>
     </div>
